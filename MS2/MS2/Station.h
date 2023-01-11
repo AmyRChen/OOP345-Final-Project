61f3769ff4f2a1b2d9da84b8a3796d070561e5d1
Ping-Ju Chen <103694860+AmyRChen@users.noreply.github.com>
2023-01-11 13:20:12 -0500

    Add files via upload

---
+++ MS2/MS2/Station.h
@@ -0,0 +1,34 @@
+// Name: PingJu, Chen
+// Seneca Student ID: 151043205
+// Seneca email: pchen72@myseneca.ca
+// Date of completion: TBA
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#ifndef SDDS_STATION_H
+#define SDDS_STATION_H
+#include <string>
+#include "Utilities.h"
+
+namespace sdds {
+   class Station {
+      int m_id{};
+      std::string m_itemName{};
+      std::string m_desc{};
+      size_t m_serialNum{};
+      size_t m_inStockNum{};
+      //Class variables
+      static unsigned m_widthField;
+      static unsigned id_generator;
+   public:
+      Station(const std::string& rec);
+      const std::string& getItemName() const;
+      size_t getNextSerialNumber();
+      size_t getQuantity() const;
+      void updateQuantity();
+      void display(std::ostream& os, bool full) const;
+   };
+}
+#endif // !SDDS_STATION_H
+
---
+++ MS2/MS2/Utilities.h
@@ -0,0 +1,30 @@
+// Name: PingJu, Chen
+// Seneca Student ID: 151043205
+// Seneca email: pchen72@myseneca.ca
+// Date of completion: TBA
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+#include <string>
+
+namespace sdds {
+   class Utilities {
+      size_t m_widthField{ 1 };
+      //Class variable 
+      static char m_delimiter;
+   public:
+      void setFieldWidth(size_t newWidth);
+      size_t getFieldWidth() const;
+      std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+      static void setDelimiter(char newDelimiter);
+      static char getDelimiter();
+   };
+
+   //Self-add func:
+   std::string trim(const std::string& str);
+}
+#endif // !SDDS_UTILITIES_H
+
